272d26c8603bd539d9d51382510c211bdfe3e624
SonDH <hs5011@gmail.com>
2025-10-01 16:08:02 +0700

    Initial commit: frontend

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true
-  }
+  base: '/task-analyzer-frontend/', // <-- CHÚ Ý: đúng tên repo FE
 })
